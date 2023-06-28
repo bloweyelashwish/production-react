13f0b0e18f80d97e49d24699a4300567b8b6f70a
beachfires <lisaxbarkar@gmail.com>
2023-06-28 22:16:48 +0300

    add app link to ui kit, restructure router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,26 +1,17 @@
 import "./styles/index.scss";
-import { Suspense } from "react";
-import { Route, Routes } from "react-router-dom";
-import { Link } from "react-router-dom";
 import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "./providers/ThemeProvider";
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
+import { AppRouter } from "./providers/router";
+import { Navbar } from "widgets/Navbar";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
     <div className={classNames("app", {}, [theme])}>
+      <Navbar />
       <button onClick={toggleTheme}>Toggle</button>
-      <Link to={"/"}>Главна</Link>
-      <Link to={"/about"}>О сайте</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/about" element={<AboutPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
