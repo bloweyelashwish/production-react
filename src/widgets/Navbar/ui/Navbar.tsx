3737e2991b1c72eccba1b61f23e1f6935e1aa2d2
beachfires <lisaxbarkar@gmail.com>
2023-06-29 22:47:32 +0300

    add svg/file loader. add theme switcher

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,7 @@
-import {classNames} from "shared/lib/classNames/classNames";
+import { classNames } from "shared/lib/classNames/classNames";
 import cls from "./Navbar.module.scss";
-import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
+import {ThemeSwitcher} from "widgets/ThemeSwitcher";
 
 interface NavbarProps {
   className?: string;
@@ -9,9 +10,18 @@ interface NavbarProps {
 export const Navbar = ({ className }: NavbarProps) => {
   return (
     <div className={classNames(cls.Navbar, {}, [className])}>
+      <ThemeSwitcher/>
       <div className={cls.links}>
-        <AppLink theme={AppLinkTheme.SECONDARY} to={"/"} className={cls.mainLink}>Главная</AppLink>
-        <AppLink theme={AppLinkTheme.SECONDARY} to={"/about"}>О сайте</AppLink>
+        <AppLink
+          theme={AppLinkTheme.SECONDARY}
+          to={"/"}
+          className={cls.mainLink}
+        >
+          Главная
+        </AppLink>
+        <AppLink theme={AppLinkTheme.SECONDARY} to={"/about"}>
+          О сайте
+        </AppLink>
       </div>
     </div>
   );
