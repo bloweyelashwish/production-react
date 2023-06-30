109684cf022caf39a955f03c972029cb962aa1ea
beachfires <lisaxbarkar@gmail.com>
2023-06-30 15:26:26 +0300

    add babel & extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -42,5 +42,21 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
     exclude: /node_modules/,
   }
 
-  return [fileLoader, svgLoader, typescriptLoader, cssLoader]
+  const babelLoader = {
+    test: /\.(js|jsx|tsx)$/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+        plugins: [
+          [
+            'i18next-extract',
+            { locales: ['ru', 'en'], keyAsDefaultValue: true },
+          ],
+        ],
+      },
+    },
+  }
+
+  return [fileLoader, svgLoader, babelLoader, typescriptLoader, cssLoader]
 }
