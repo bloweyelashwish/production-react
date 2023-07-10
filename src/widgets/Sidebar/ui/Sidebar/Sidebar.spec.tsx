90d421f9fe83a2a85616be28144e0023ed0c8f1f
beachfires <lisaxbarkar@gmail.com>
2023-07-10 15:35:14 +0300

    fix tests

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.spec.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.spec.tsx
@@ -1,15 +1,15 @@
 import { Sidebar } from './Sidebar';
 import { fireEvent, screen } from '@testing-library/react';
-import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
 
 describe('Sidebar', () => {
   test('sidebar renders', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
   });
 
   test('sidebar toggles', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     const toggleButton = screen.getByTestId('sidebar-toggle');
     fireEvent.click(toggleButton);
     expect(screen.getByTestId('sidebar')).toHaveClass('collapsed');
