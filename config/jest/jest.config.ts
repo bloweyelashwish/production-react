e39095308916d2a6951105a9ced88f8843a4a5b3
beachfires <lisaxbarkar@gmail.com>
2023-07-10 14:15:18 +0300

    add screenshot tests to ci

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -12,9 +12,7 @@ export default {
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
   moduleDirectories: ['node_modules'],
   modulePaths: ['<rootDir>src'],
-  testMatch: [
-    '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
-  ],
+  testMatch: ['<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)'],
   rootDir: '../../',
   setupFilesAfterEnv: ['<rootDir>config/jest/setupTests.ts'],
   moduleNameMapper: {
